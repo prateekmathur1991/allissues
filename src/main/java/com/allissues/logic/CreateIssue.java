5d5db1da0a701fe455b1483f6b37cebee63e18c3
Prateek <prateekmathur1991@gmail.com>
2015-03-02 17:21:08 +0530

    Created CreateIssue servlet

---
+++ src/main/java/com/allissues/logic/CreateIssue.java
@@ -0,0 +1,33 @@
+package com.allissues.logic;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateIssue
+ */
+public class CreateIssue extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String title = request.getParameter("title");
+			String description = request.getParameter("description");
+			int priority = Integer.parseInt(request.getParameter("priority"));
+			Date deadline = new SimpleDateFormat("yyyy/mm/dd").parse(request.getParameter("deadline"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/main/java/com/allissues/logic/RegisterServlet.java
+++ src/main/java/com/allissues/logic/RegisterServlet.java
@@ -1,6 +1,5 @@
 package com.allissues.logic;
 
-import static com.allissues.service.OfyService.factory;
 import static com.allissues.service.OfyService.ofy;
 
 import java.io.IOException;
@@ -19,22 +18,7 @@ import com.allissues.data.Developer;
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
+   /**
 	 * The doPost method handles the user request when it comes to the server
 	 * 
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -48,7 +32,7 @@ public class RegisterServlet extends HttpServlet {
 		
 		System.out.println(accountType);
 		
-		if (accountType == "Developer")	{
+		if (accountType.equalsIgnoreCase("Developer"))	{
 			Developer developer = new Developer(email, firstName, lastName, password);
 			ofy().save().entity(developer).now();
 		}
