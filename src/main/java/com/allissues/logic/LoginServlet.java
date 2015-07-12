b0a28d01701d94c75fb24abd1c840547947c65fd
Prateek Mathur <prateekmathur1991@gmail.com>
2015-07-12 16:09:59 +0530

    Work on login action in progress

--- src/main/java/com/allissues/logic/LoginServlet.java
+++ src/main/java/com/allissues/logic/LoginServlet.java
@@ -1,10 +1,13 @@
 package com.allissues.logic;
 
 import static com.allissues.service.OfyService.ofy;
+
 import com.allissues.data.Customer;
 import com.allissues.data.Developer;
+import com.googlecode.objectify.Key;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -32,10 +35,37 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String username = request.getParameter("username");
+		String email = request.getParameter("email");
 		String password = request.getParameter("password");
+		
+		Customer customer = null;
+		Developer developer = null;
 
-		// Load the entity from datastore
+		// Try to load Customer entity from datastore
+		customer = ofy().load().key(Key.create(Customer.class, email)).now();
+		
+		if (null == customer)	{
+			developer = ofy().load().key(Key.create(Developer.class, email)).now();
+			if (developer != null)	{
+				if (developer.getPassword().equals(password))	{
+					// Developer login successful
+					// Set the username for session tracking
+					request.setAttribute("username", developer.getName());
+					request.getRequestDispatcher("Home.jsp").forward(request, response);
+				}
+			}
+		} else	{
+			if (customer.getPassword().equals(password)) {
+				// Customer login successful
+				// Set the username for session tracking
+				request.setAttribute("username", customer.getName());
+				request.getRequestDispatcher("Home.jsp").forward(request, response);
+			}
+		}
+		
+		// Login Faliure
+		PrintWriter out = response.getWriter();
+		out.println("loginfaliure");
 		
 	}
 
