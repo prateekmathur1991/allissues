3163c1ea5432d34192faf0da4957753cc6020755
Prateek Mathur <prateekmathur1991@gmail.com>
2015-09-27 14:41:42 +0530

    Corrections in js code for settings

--- src/main/webapp/js/settings.js
+++ src/main/webapp/js/settings.js
@@ -10,6 +10,7 @@ $(document).ready(function (e) {
 			if ($(this).attr('id') == 'oldpass' || $(this).attr('id') == 'newpass') {
 				if ($.trim($(this).val()) == '') {
 					$(this).closest('.form-group').addClass('has-error');
+					$('#error').addClass('alert-danger').removeClass('alert-success');
 					$('#error').html('This field cannot be left blank');
 					$('#error').slideDown();
 					$(this).focus();
@@ -23,6 +24,7 @@ $(document).ready(function (e) {
 			} else if ($(this).attr('id') == 'confpass') {
 				if ($.trim($('#newpass').val()) != $.trim($('#confpass').val())) {
 					$('#confpass').closest('.form-group').addClass('has-error');
+					$('#error').addClass('alert-danger').removeClass('alert-success');
 					$('#error').html('The passwords do not match. Please enter the same value for new and confirm password');
 					$('#error').slideDown();
 					$(this).focus();
@@ -30,35 +32,36 @@ $(document).ready(function (e) {
 					return false;
 				}
 			}
-			
-			if (check) {
-				$.post('/update', {action: 'updatepass', oldpass: $('#oldpass').val(), newpass: $('#newpass').val()}, function (data) {
-					if (data.status == 'success') {
-						$('#error').addClass('alert-success').removeClass('alert-danger');
-						$('#error').html('Your password has been updated successfully.');
+		});
+		
+		if (check) {
+			$.post('/update', {action: 'updatepass', oldpass: $('#oldpass').val(), newpass: $('#newpass').val()}, function (data) {
+				if (data.status == 'success') {
+					$('#error').addClass('alert-success').removeClass('alert-danger');
+					$('#error').html('Your password has been updated successfully.');
+					$('#error').slideDown();
+					setTimeout(function () {
+						$('#error').slideUp();
+					}, 5000);
+				} else if (data.status == 'failure') {
+					if (data.reason == 'oldnomatch') {
+						$('#error').addClass('alert-danger').removeClass('alert-success');
+						$('#error').html('The current password you have entered is not correct. Please enter the password you are using currently');
 						$('#error').slideDown();
+						$('#oldpass').focus();
 						setTimeout(function () {
 							$('#error').slideUp();
 						}, 5000);
-					} else if (data.status == 'failure') {
-						if (data.reason == 'oldnomatch') {
-							$('#error').addClass('alert-danger').removeClass('alert-success');
-							$('#error').html('The current password you have entered is not correct. Please enter the password you are using currently');
-							$('#error').slideDown();
-							$('#oldpass').focus();
-							setTimeout(function () {
-								$('#error').slideUp();
-							}, 5000);
-						}
 					}
-				});
-			}
-		});
+				}
+			});
+		}
 	});
 	
 	$('#save-name-button').on('click', function () {
 		var check = true;
 		if ($.trim($('#name').val()) == '') {
+			$('#error').addClass('alert-danger').removeClass('alert-success');
 			$('#error').html('Please enter a value for display name');
 			$('#error').slideDown();
 			$('#name').focus();
